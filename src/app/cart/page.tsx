537b24aeac67fed98f4d36afec4f2f0727209d73
David Carrero <davidcarrero@MacBook-Pro-de-David.local>
2025-02-08 14:18:32 -0800

    fix/cart-use-client-implemented

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,9 @@
-"use server";
+"use client";
 
 import Breadcrumb from "@/components/Atoms/Breadcrumb/Breadcrumb";
 import CartContainer from "@/components/Organisms/CartContainer/CartContainer";
 
-export default async function Cart() {
+export default function Cart() {
   return (
     <main className="flex flex-col py-8 px-32 max-sm:py-8 max-sm:px-6">
       <Breadcrumb textContent={"Back to Catalog"} href={"/"} />
